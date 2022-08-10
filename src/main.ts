5c86f3f485e3b2e175d95f71285df8008c32ea52
Raluca Nicola <RNicola@esri.com>
2022-08-10 17:17:39 +0200

    Add initial files

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,214 @@
-import Color from "@arcgis/core/Color";
-import { whenOnce } from "@arcgis/core/core/reactiveUtils";
-import SceneView from "@arcgis/core/views/SceneView";
-import WebScene from "@arcgis/core/WebScene";
-import "@esri/calcite-components/dist/calcite/calcite.css";
-import "@esri/calcite-components/dist/components/calcite-loader";
+import MapView from "@arcgis/core/views/MapView";
+import WebMap from "@arcgis/core/WebMap";
+import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
+import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol";
+import LineLayerAnimation from "./lib/LineLayerAnimation";
+import FeatureEffect from "@arcgis/core/layers/support/FeatureEffect";
+import FeatureFilter from "@arcgis/core/layers/support/FeatureFilter";
+import {SimpleMarkerSymbol, TextSymbol} from "@arcgis/core/symbols";
+import LabelClass from "@arcgis/core/layers/support/LabelClass";
+import Compass from "@arcgis/core/widgets/Compass";
+import UniqueValueRenderer from "@arcgis/core/renderers/UniqueValueRenderer";
 
-// setAssetPath("https://js.arcgis.com/calcite-components/1.0.0-beta.77/assets");
-
-// const params = new URLSearchParams(document.location.search.slice(1));
-// const someParam = params.has("someParam");
-
-// IdentityManager.registerOAuthInfos([
-//   new OAuthInfo({
-//     appId: "",
-//     popup: true,
-//     popupCallbackUrl: `${document.location.origin}${document.location.pathname}oauth-callback-api.html`,
-//   }),
-// ]);
-
-// (window as any).setOAuthResponseHash = (responseHash: string) => {
-//   IdentityManager.setOAuthResponseHash(responseHash);
-// };
-
-const map = new WebScene({
+const map = new WebMap({
   portalItem: {
-    id: "91b46c2b162c48dba264b2190e1dbcff",
+    id: "02bae9c8de294eabaa91972b14394ecc",
   },
 });
 
-const view = new SceneView({
+const view = new MapView({
   container: "viewDiv",
   map,
-  qualityProfile: "high",
+  ui: {
+    components: [],
+  },
 });
 
-map.when().then(() => {
-  map.ground.surfaceColor = new Color([220, 220, 220]);
+let compass = new Compass({
+  view: view,
 });
 
-map.loadAll().then(() => {
-  const slides = map.presentation.slides;
-  const slide = slides.getItemAt(Math.floor(Math.random() * slides.length));
-  slide.applyTo(view, { animate: false });
+// adds the compass to the top left corner of the MapView
+view.ui.add(compass, "top-left");
+
+const pois = new GeoJSONLayer({
+  url: "./data/points.geojson",
+  renderer: new SimpleRenderer({
+    symbol: new SimpleMarkerSymbol({
+      color: [255, 165, 0, 1],
+      size: 8,
+      style: "circle",
+      outline: {
+        width: 8,
+        color: [255, 165, 0, 0.3],
+      },
+    }),
+  }),
+  labelingInfo: [
+    new LabelClass({
+      labelExpressionInfo: {expression: "$feature.name"},
+      labelPlacement: "center-right",
+      symbol: new TextSymbol({
+        color: [255, 165, 0, 1],
+        haloSize: 2,
+        haloColor: [255, 255, 255, 1],
+        font: {
+          size: 10,
+        },
+      }),
+    }),
+  ],
 });
 
-whenOnce(() => !view.updating).then(() => {
-  const loader = document.getElementById("loader");
-  loader?.parentElement?.removeChild(loader);
+const filterFeatures = (filter: string) => {
+  pois.featureEffect = new FeatureEffect({
+    filter: new FeatureFilter({
+      where: filter,
+    }),
+    excludedEffect: "grayscale(100%) opacity(30%)",
+  });
+};
+
+map.add(pois);
+
+const hikingLayer = new GeoJSONLayer({
+  url: "./data/hike.geojson",
+  renderer: new UniqueValueRenderer({
+    field: "type",
+    defaultSymbol: new SimpleLineSymbol({
+      width: 3,
+      color: [252, 169, 3],
+      style: "short-dash",
+      cap: "round",
+      join: "round",
+    }),
+    uniqueValueInfos: [
+      {
+        value: "road",
+        symbol: new SimpleLineSymbol({
+          width: 3,
+          color: [252, 169, 3],
+          style: "solid",
+          cap: "round",
+          join: "round",
+        }),
+      },
+    ],
+  }),
+  visible: false,
 });
 
-window["view"] = view;
+map.add(hikingLayer);
+
+const displayHike = (value: boolean) => {
+  hikingLayer.visible = value;
+};
+
+const route = new GeoJSONLayer({
+  url: "./data/train-track.geojson",
+  renderer: new SimpleRenderer({
+    symbol: new SimpleLineSymbol({
+      width: 3,
+      color: [252, 169, 3],
+      style: "solid",
+      cap: "round",
+      join: "round",
+    }),
+  }),
+});
+
+const animation = new LineLayerAnimation({
+  sourceLayer: route,
+});
+
+animation.whenAnimatedLayer().then(animatedLayer => {
+  map.add(animatedLayer);
+});
+
+function getScrollProgress(element: HTMLElement) {
+  const elemRect = element.getBoundingClientRect();
+
+  const top = elemRect.top;
+  // map is covering up 30% of the window height
+  const windowHeight = 0.65 * window.innerHeight || document.documentElement.clientHeight;
+
+  const progress = Math.min(Math.max(windowHeight - top, 0.01), elemRect.height);
+  return progress / elemRect.height;
+}
+
+interface SectionInfo {
+  id: string;
+  top: number;
+  percentageTop: number;
+}
+
+let sectionsInfo: SectionInfo[] = [];
+const sectionAnimations = {
+  "section-1": 0,
+};
+let currentSectionId: null | string = null;
+let previousSectionId: null | string = null;
+
+const setSection = (section: string) => {
+  switch (section) {
+    case "section-0":
+      filterFeatures(`name IN ('Coronado Island', 'User Conference')`);
+      view.goTo(map.bookmarks.getItemAt(0).viewpoint, {duration: 1500});
+      displayHike(false);
+      break;
+    case "section-1":
+      filterFeatures(`1=2`);
+      view.goTo(map.bookmarks.getItemAt(1).viewpoint, {duration: 1500});
+      displayHike(false);
+      break;
+    case "section-2":
+      filterFeatures(`name IN ('Hollywood Bowl', 'Griffith Observatory', 'Santa Monica beach', 'Walk of Fame')`);
+      view.goTo(map.bookmarks.getItemAt(2).viewpoint, {duration: 1500});
+      displayHike(false);
+      break;
+  }
+};
+
+const render = () => {
+  if (currentSectionId) {
+    if (currentSectionId === previousSectionId) {
+      if (typeof sectionAnimations[currentSectionId] !== "undefined") {
+        route.queryObjectIds().then(function (objectIds) {
+          const id = sectionAnimations[currentSectionId as string];
+          const routeObjectId = objectIds[id];
+          if (typeof routeObjectId !== "undefined" && currentSectionId) {
+            const scrollProgress = getScrollProgress(document.getElementById(currentSectionId) as HTMLElement);
+            animation.seek(scrollProgress, routeObjectId);
+          }
+        });
+      }
+    } else {
+      previousSectionId = currentSectionId;
+      setSection(currentSectionId);
+    }
+  }
+};
+
+const calculateSectionsInfo = () => {
+  const sectionsList = document.querySelectorAll("section");
+  const sectionsArray = Array.from(sectionsList);
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  sectionsInfo = sectionsArray.map(section => {
+    const sectionRect = section.getBoundingClientRect();
+    const top = sectionRect.top;
+    const percentageTop = top / windowHeight;
+    if (percentageTop < 0.7) {
+      currentSectionId = section.id;
+    }
+    return {
+      id: section.id,
+      top,
+      percentageTop,
+    };
+  });
+};
+window.onscroll = e => {
+  calculateSectionsInfo();
+  render();
+};
